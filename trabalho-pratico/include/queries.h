78d17b728ad8a630794f86b908a4a4c396543d7f
Rafael Vilas Boas <rafaelvilasboas0102@gmail.com>
2023-11-08 19:29:12 +0000

    Adicionado header file de queries

---
+++ trabalho-pratico/include/queries.h
@@ -0,0 +1,26 @@
+#ifndef _QUERIES_
+#define _QUERIES_
+
+#include "utility.h"
+//retornos (estrutura ?)
+void Q1(char *id);
+
+void Q2(char *id, Q2Type type);
+
+void Q3(char *id);
+
+void Q4(char *id);
+
+void Q5(char *name, Date begin, Date end);
+
+void Q6(int year, int N);
+
+void Q7(int N);
+
+void Q8(char *id, Date begin, Date end);
+
+void Q9(char *prefix);
+
+void Q10(int year, char month);
+
+#endif
